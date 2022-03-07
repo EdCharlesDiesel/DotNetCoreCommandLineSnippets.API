93a257a1d2903dc3ba554c514e9c058ea7bd5b99
Khotso Mokhethi <Mokhetkc@hotmail.com>
2022-03-08 00:22:32 +0200

    Added Command Model to API Project

---
+++ DotNetCoreCommandLineSnippets.API/Models/Command.cs
@@ -0,0 +1,18 @@
+namespace DotNetCoreCommandLineSnippets.API.Models;
+
+public class Command
+ {
+   [Key]
+   [Required]
+   public int Id {get; set;}
+ 
+   [Required]
+   [MaxLength(250)]
+   public string HowTo {get; set;}
+    
+   [Required]
+   public string Platform {get; set;}
+ 
+   [Required]
+   public string CommandLine {get; set;}
+ }
