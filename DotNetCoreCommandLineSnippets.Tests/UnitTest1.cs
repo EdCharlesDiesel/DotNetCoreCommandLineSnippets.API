fb0a722a9db4ab285d4d9877788881ed925535c9
Khotso Mokhethi <Mokhetkc@hotmail.com>
2022-03-12 07:10:56 +0200

    Fixed Unit tests project

--- DotNetCoreCommandLineSnippets.Tests/UnitTest1.cs
+++ DotNetCoreCommandLineSnippets.Tests/UnitTest1.cs
@@ -1,12 +1,28 @@
-using Xunit;
+// using Xunit;
 
-namespace DotNetCoreCommandLineSnippets.Tests;
+// namespace DotNetCoreCommandLineSnippets.Tests
+// {
+// public class UnitTest1
+// {
+//     [Fact]
+//     public void Test1()
+//     {
 
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
+//     }
+// }
+// }
 
-    }
-}
+// using System;
+// using Xunit;
+
+// namespace CommanAPI.Tests
+// {
+//     public class UnitTest1
+//     {
+//         [Fact]
+//         public void Test1()
+//         {
+
+//         }
+//     }
+// }
